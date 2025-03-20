2725680104238e36d4e49cce3a7df14950910a3f
lull-y-miyazaki <y.miyazaki@lull-inc.co.jp>
2025-03-20 12:50:21 +0900

    セッション管理を実装

--- src/main/java/com/example/demo/model/Account.java
+++ src/main/java/com/example/demo/model/Account.java
@@ -1,5 +1,38 @@
 package com.example.demo.model;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
 public class Account {
+
+	// フィールド
+	private Integer id;
+	private String name;
+
+	// コンストラクタ
+	public Account() {
+	}
+
+	public Account(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	// ゲッター＆セッター
+	public String getName() {
+
+		return name;
+	}
+
+	public Integer getId() {
+
+		return id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
