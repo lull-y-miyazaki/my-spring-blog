9afa49118f3c9137c8506e78498a40e85cdd7f3a
lull-y-miyazaki <y.miyazaki@lull-inc.co.jp>
2025-03-24 19:17:48 +0900

    ログイン処理の実装

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.example.demo.controller;
 
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.User;
@@ -16,7 +17,10 @@ public class UserController {
 
 	// 必要なオブジェクトを自動的に生成・管理するためのアノテーション
 	@Autowired
-	UserRepository userRepository;
+	private UserRepository userRepository;
+
+	@Autowired
+	private HttpSession session;
 
 	// 新規ユーザー登録画面の表示
 	@GetMapping("/users/add")
@@ -48,15 +52,37 @@ public class UserController {
 			@RequestParam(defaultValue = "") String error,
 			Model model) {
 
+		// セッション情報をクリア
+		session.invalidate();
+
 		return "login";
 	}
 
 	// ログイン処理
 	@PostMapping("/login")
 	public String login(
-			@RequestBody String entity) {
+			@RequestParam String email,
+			@RequestParam String password,
+			Model model) {
 
-		return entity;
+		// email または password が空の場合にエラーメッセージを表示
+		if (email == null || email.isEmpty() || password == null || password.isEmpty()) {
+			model.addAttribute("message", "メールアドレスとパスワードを入力してください");
+			return "login";
+		}
+
+		// データーベースからユーザー情報を取得
+		User user = userRepository.findByEmailAndPassword(email, password);
+
+		// email または password が正しくない場合にエラーメッセージを表示
+		if (user == null) {
+			model.addAttribute("message", "メールアドレスまたはパスワードが正しくありません");
+			return "login";
+		}
+
+		session.setAttribute("user", user);
+
+		return "redirect:/blogs";
 	}
 
 }
