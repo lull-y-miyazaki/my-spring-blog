94a2c2bb3191a872794d73e2f5a2405cfb40bb91
lull-y-miyazaki <y.miyazaki@lull-inc.co.jp>
2025-03-24 16:41:07 +0900

    新規ユーザー登録画面の表示

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,8 +1,23 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.repository.UserRepository;
 
 @Controller
 public class UserController {
+
+	// 必要なオブジェクトを自動的に生成・管理するためのアノテーション
+	@Autowired
+	UserRepository userRepository;
+
+	// 新規ユーザー登録画面の表示
+	@GetMapping("/users/add")
+	public String create() {
+
+		return "addUser";
+	}
 
 }
