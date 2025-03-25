796a8b7a7a827e2f5bf0d7ba545201f2a98ee727
lull-y-miyazaki <y.miyazaki@lull-inc.co.jp>
2025-03-25 15:05:54 +0900

    ブログ新規作成画面の表示と作成処理の実装

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -6,15 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Blog;
 import com.example.demo.entity.Category;
+import com.example.demo.entity.User;
+import com.example.demo.model.Account;
 import com.example.demo.repository.BlogRepository;
 import com.example.demo.repository.CategoryRepository;
+import com.example.demo.repository.UserRepository;
 
 @Controller
 public class BlogController {
+
+	@Autowired
+	private Account account;
+
+	@Autowired
+	private UserRepository userRepository;
 
 	@Autowired
 	private CategoryRepository categoryRepository;
@@ -44,6 +54,38 @@ public class BlogController {
 		model.addAttribute("blogs", blogList);
 
 		return "blogs";
+	}
+
+	// ブログの新規登録画面の表示
+	@GetMapping("/blogs/add")
+	public String create(Model model) {
+
+		// 全カテゴリーの取得
+		List<Category> categoryList = categoryRepository.findAll();
+		model.addAttribute("categories", categoryList);
+
+		return "addBlog";
+	}
+
+	// ブログの新規登録処理
+	@PostMapping("/blogs/add")
+	public String add(
+			@RequestParam Integer categoryId,
+			@RequestParam String title,
+			@RequestParam String content,
+			Model model) {
+
+		// カテゴリー情報の取得
+		// orElseThrow()：中身が「ある」なら取り出して、「ない」なら例外を投げる
+		Category category = categoryRepository.findById(categoryId).orElseThrow();
+
+		// ログインしているユーザー情報の取得
+		User user = userRepository.findById(account.getId()).orElseThrow();
+
+		Blog blog = new Blog(category, user, title, content);
+		blogRepository.save(blog);
+
+		return "redirect:/blogs";
 	}
 
 	// ブログ変更画面の表示
