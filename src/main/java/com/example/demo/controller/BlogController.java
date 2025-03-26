9bfefc5728a917cd57537a15e1c2af729c2a62db
lull-y-miyazaki <y.miyazaki@lull-inc.co.jp>
2025-03-26 18:01:48 +0900

    ブログ記事の編集画面の表示・編集処理の実装

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -82,7 +83,10 @@ public class BlogController {
 		// ログインしているユーザー情報の取得
 		User user = userRepository.findById(account.getId()).orElseThrow();
 
+		// ブログ新規作成用のインスタンスを生成
 		Blog blog = new Blog(category, user, title, content);
+
+		// 引数のエンティティをDBに保存
 		blogRepository.save(blog);
 
 		return "redirect:/blogs";
@@ -90,10 +94,47 @@ public class BlogController {
 
 	// ブログ変更画面の表示
 	@GetMapping("/blogs/edit")
-	public String update(
+	public String edit(
 			@RequestParam Integer blogId,
 			Model model) {
+
+		// 全カテゴリーの取得
+		List<Category> categoryList = categoryRepository.findAll();
+
+		// ID（主キー）で検索してブログ情報を取得
+		Blog blog = blogRepository.findById(blogId).get();
+
+		// Map.of()の簡易Mapで複数のデータをまとめて送る
+		model.addAllAttributes(Map.of(
+				"categories", categoryList,
+				"blog", blog));
+
 		return "editBlog";
+	}
+
+	// ブログ更新処理
+	@PostMapping("/blogs/update")
+	public String update(
+			@RequestParam Integer blogId,
+			@RequestParam Integer categoryId,
+			@RequestParam String title,
+			@RequestParam String content,
+			Model model) {
+
+		// カテゴリー情報の取得
+		// orElseThrow()：中身が「ある」なら取り出して、「ない」なら例外を投げる
+		Category category = categoryRepository.findById(categoryId).orElseThrow();
+
+		// ログインしているユーザー情報の取得
+		User user = userRepository.findById(account.getId()).orElseThrow();
+
+		// ブログ更新用インスタンスを生成
+		Blog blog = new Blog(blogId, category, user, title, content);
+
+		// 引数のエンティティをDBに保存
+		blogRepository.save(blog);
+
+		return "redirect:/blogs";
 	}
 
 }
